16b46b21a2916df5c03a4c0835b518840f3a6d48
Cosmin Cojocar <cosmin.cojocar@gmx.ch>
2018-08-27 18:31:08 +0200

    feat: get the user info from the dex with the token forwared by the sso proxy

--- main.go
+++ main.go
@@ -1,33 +1,66 @@
 package main
 
 import (
+	"errors"
 	"fmt"
-	"log"
+	"io/ioutil"
 	"net/http"
-	"strings"
 )
 
+const TOKEN_HEADER = "X-Forwarded-Access-Token"
+const DEX_USERINFO = "https://dex.sso.jx.cosmin.rawlings.it/userinfo"
+
 func handler(w http.ResponseWriter, r *http.Request) {
-	title := "Jenkins X golang http example"
-
-	from := ""
-	if r.URL != nil {
-		from = r.URL.String()
-	}
-	if from != "/favicon.ico" {
-		log.Printf("title: %s\n", title)
+	token, err := parseAccessToken(r)
+	if err != nil {
+		printError(w, err)
+		return
 	}
 
-	printHeaders(r)
+	userinfo, err := getUserinfo(token)
+	if err != nil {
+		printError(w, err)
+		return
+	}
 
-	fmt.Fprintf(w, "Hello from:  "+title+"\n")
+	fmt.Fprintf(w, "User Info:\n%s", userinfo)
 }
 
-func printHeaders(r *http.Request) {
-	fmt.Println("Headers:")
+func printError(w http.ResponseWriter, err error) {
+	fmt.Fprintf(w, "Error: %v", err)
+}
+
+func parseAccessToken(r *http.Request) (string, error) {
 	for k, v := range r.Header {
-		fmt.Printf("%s: %s\n", k, strings.Join(v, " "))
+		if k == TOKEN_HEADER {
+			return v[0], nil
+		}
 	}
+	return "", errors.New("no token found")
+}
+
+func getUserinfo(accessToken string) (string, error) {
+	client := http.Client{}
+	req, err := http.NewRequest("GET", DEX_USERINFO, nil)
+	if err != nil {
+		return "", err
+	}
+	req.Header.Add("Authorization", "bearer "+accessToken)
+	res, err := client.Do(req)
+	if err != nil {
+		return "", err
+	}
+	defer res.Body.Close()
+
+	if res.StatusCode != 200 {
+		return "", fmt.Errorf("status code: %d", res.StatusCode)
+	}
+
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		return "", err
+	}
+	return string(body), nil
 }
 
 func main() {
